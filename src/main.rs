ad9810dcb45ac31799505e741499b0c619f4902b
drsh4dow <daniel.morettiv@gmail.com>
2022-05-28 02:17:23 -0400

    nothing new really, just some comments

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+    format!("Hello {}!\n", &name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // HttpServer it is the struct responsible for all
+    // the transport level concerns.
     HttpServer::new(|| {
+        // App it is where all the application logic lives
+        // This includes routing, middleware, request handlers, etc
+        // The app struct uses a fluent API.
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
